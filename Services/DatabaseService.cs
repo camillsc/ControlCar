aa9e0450fc27664c2fbd635f0045539c33f343c7
Camille <christelihcamille@hotmail.com>
2025-06-03 17:42:51 -0300

    Criando ImageService

--- Services/DatabaseService.cs
+++ Services/DatabaseService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using PCLExt.FileStorage.Folders;
+﻿using PCLExt.FileStorage.Folders;
 using SQLite;
 
 namespace ControlCar.Services
---
+++ Services/ImageService.cs
@@ -0,0 +1,38 @@
+﻿namespace ControlCar.Services
+{
+
+    public static class ImageService
+    {
+        public static async Task<string> SelecionarImagem()
+        {
+            string diretorio = "";
+
+            var imagemSelecionada = await MediaPicker.PickPhotoAsync();
+
+            if (imagemSelecionada != null)
+            {
+                diretorio = imagemSelecionada.FullPath;
+            }
+
+            return diretorio;
+        }
+
+        public static string CopiarImagem(string sDirOriginal)
+        {
+            var DiretorioDestino = "";
+
+            var novoDiretorio = Path.Combine(AppContext.BaseDirectory, "Imagens");
+
+            if (!Directory.Exists(novoDiretorio))
+            {
+                Directory.CreateDirectory(novoDiretorio);
+            }
+
+            DiretorioDestino = Path.Combine(novoDiretorio, Path.GetFileName(sDirOriginal));
+
+            File.Copy(sDirOriginal, DiretorioDestino, overwrite: true);
+
+            return DiretorioDestino;
+        }
+    }
+}
